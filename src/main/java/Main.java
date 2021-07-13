5f0b914a2c3e77447ec2b9d23031ebb64189a861
yijia-cc <bestmissjia@gmail.com>
2021-07-13 00:07:42 -0700

    Implement listInventories API

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 import ui.VendingMachineUI;
 import ui.terminal.VendingMachineTerminalUI;
+import vending_machine.Inventory;
 import vending_machine.VendingMachineAutomator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         VendingMachineUI vendingMachineUI = new VendingMachineTerminalUI();
-        VendingMachineAutomator vendingMachineAutomator = new VendingMachineAutomator(vendingMachineUI);
+        List<Inventory> inventories = new ArrayList<>();
+        VendingMachineAutomator vendingMachineAutomator = new VendingMachineAutomator(vendingMachineUI, inventories);
         vendingMachineAutomator.run();
     }
 }
