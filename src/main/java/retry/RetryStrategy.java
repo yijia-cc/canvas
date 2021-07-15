699522a89865778acf087d735b50fa8515599ecf
yijiacc <76569613+yijia-cc@users.noreply.github.com>
2021-07-14 17:21:38 -0700

    Add instant retry (#3)
    * Add retry unit test
    
    * Implement instant retry
    
    * Update src/test/java/retry/RetryStrategyTest.java

---
+++ src/main/java/retry/RetryStrategy.java
@@ -0,0 +1,14 @@
+package retry;
+
+public class RetryStrategy {
+    public static boolean instant(RetryCallback callBack, int retryLimit) {
+        for (int retry = 0; retry < retryLimit; retry++) {
+            try {
+                callBack.execute();
+                return true;
+            } catch (Exception ignore){
+            }
+        }
+        return false;
+    }
+}
